f5097cbb0be2eb419aec175a8bed3ea3e41199e1
Moon-R <zhangmuren1991@163.com>
2017-05-09 19:11:16 +0800

    Add files via upload

--- webpack.config.dist.js
+++ webpack.config.dist.js
@@ -1,44 +1,47 @@
+require('babel-polyfill');
 var path = require('path');
 var webpack = require('webpack');
 var ExtractTextPlugin = require('extract-text-webpack-plugin'); //css单独打包
 var HtmlWebpackPlugin = require('html-webpack-plugin'); //生成html
 
+
 var ROOT_PATH = path.resolve(__dirname);
 var APP_PATH = path.resolve(ROOT_PATH, 'src'); //__dirname 中的src目录，以此类推
 var APP_FILE = path.resolve(APP_PATH, 'index.js'); //根目录文件app.jsx地址
-var BUILD_PATH = './bundle/dist'; //发布文件所存放的目录
+var BUILD_PATH = './dist/app'; //发布文件所存放的目录
 
 module.exports = {
   entry: {
-    app: APP_FILE,
+    app: ['babel-polyfill', APP_FILE],
     common: [
-            "react",
-            'react-dom',
-            'react-router-redux',
-            'redux',
-            'react-redux',
-            'redux-thunk',
-            'reselect',
-            'jquery',
-            'react-router',
-            'isomorphic-fetch'
-        ] 
+      'babel-polyfill',
+      'react',
+      'react-dom',
+      'react-router-redux',
+      'redux',
+      'react-redux',
+      'redux-thunk',
+      'reselect',
+      'jquery',
+      'react-router',
+      'isomorphic-fetch'
+    ],
+    UI: [
+      'antd'
+    ]
   },
-  UI: [
-    'antd'
-  ]
   output: {
-    publicPath: '/dist/', //编译好的文件，在服务器的路径,这是静态资源引用路径
+    publicPath: '/app/', //编译好的文件，在服务器的路径,这是静态资源引用路径
     path: BUILD_PATH, //编译到当前目录
     filename: '[name].js', //编译后的文件名字
-    chunkFilename: '[name].[chunkhash:5].min.js',
+    chunkFilename: '[name].[chunkhash:5].min.js'
   },
   module: {
     loaders: [
       {
         test: /\.jsx?$/,
         exclude: /^node_modules$/,
-        loader: ['babel'],
+        loader: ['es3ify-loader'],
         query: {
           presets: ['es2015','react'],
           plugins: ['transform-decorators-legacy']
@@ -52,6 +55,7 @@ module.exports = {
             [
               'react-css-modules',
               {
+                // APP_PATH,
                 generateScopedName: '[name]__[local]'
               }
             ]
@@ -91,15 +95,16 @@ module.exports = {
         template: './src/template/index.html', //html模板路径
         inject: 'body',
         hash: true,
-        // favicon:'./src/template/favicon.ico',
+        favicon:'./src/template/favicon.ico',
     }),
     new ExtractTextPlugin('[name].css'),
     new webpack.optimize.CommonsChunkPlugin("common", "common.bundle.js"),
     new webpack.optimize.UglifyJsPlugin({
         output: {
-          comments: false, // remove all comments
+          comments: false
         },
         compress: {
+          properties: false,
           warnings: false
         }
     })
